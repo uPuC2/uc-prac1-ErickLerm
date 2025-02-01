0037c370be772d5a60d681c1540b3522930ce43f
ErickLerm <143383868+ErickLerm@users.noreply.github.com>
2025-01-31 19:22:46 -0800

    Update Prac1.c

--- Prac1.c
+++ Prac1.c
@@ -27,9 +27,32 @@ int main()
 // OR 1011 0000 1011 0000  (mascara para lograrlo) = (1<<15) | (3<<12) | (1<<7) | (3<<4)
 //------------------------------------------------
 //    1011 0000 1011 0000  (valor que se desea)
+
+//1 B0B0
     a |= (1<<15) | (3<<12) | (1<<7) | (3<<4);
     printf("\na = %X", a);
-    
+//2 BABA  
+    a |= (0xA<<8) | (0xA<<0) ;
+    printf("\na = %X", a);
+//3 BEBE 
+    a |= (1<<10) | (1<<2) ;
+    printf("\na = %X", a);
+//4 FE00
+    a ^= (4<<12)|(0xBE)  ;    
+    printf("\na = %X", a);
+//5 FEAA
+    a |= (0xA<<4) | (0xA<<0) ;
+    printf("\na = %X", a);
+//6 0155
+    a ^= (0xFFFF);
+    printf("\na = %X", a);
+//7 0001   
+    a ^= (0x0154);
+    printf("\na = %X", a);
+//8 0100
+    a ^= 0x0101;
+    printf("\na = %X", a);
+  
 //2 BABA
 //3 BEBE
 //4 FE00
